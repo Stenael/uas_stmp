543c53dabe7effc58638c0db09fdb712d8a5b50b
StevenLabina <126638426+StevenLabina@users.noreply.github.com>
2024-01-01 21:23:59 +0700

    Steven
    Screens

---
+++ screens/signin.js
@@ -0,0 +1,33 @@
+import { StyleSheet, View, Text, Image, ImageBackground, ScrollView } from "react-native";
+import React from "react";
+
+export default function About() {
+   return (
+    
+    );
+  }
+  
+  
+  const styles = StyleSheet.create({
+    vparent:{
+      flex: 1,
+      justifyContent: 'center',
+    },
+    vparent2:{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'row'
+    },
+    imgkucing: {
+      width: 300,
+      height: 400,
+      marginRight:10,
+      marginBottom : 10,
+    },
+    imgmissing: {
+      width: 300,
+      height: 140,
+    },
+    });
+  
